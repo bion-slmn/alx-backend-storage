21f127137859ba3242a010c0aafc073856cd5a26
bion-slmn <bionsol25@gmail.com>
2024-01-24 18:59:30 +0000

    number 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -23,3 +23,29 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[
+            int, str, float, bytes]:
+        """
+            Retrieve a value from the cache.
+
+            Parameters:
+              - key (str): The key to retrieve from the cache.
+              - fn (Optional[Callable]): callable will be used to convert
+              the data back to the desired format
+            Returns:
+              - The value associated with the key, or the result
+              of calling the provided function.
+        """
+        value = self._redis.get(key)
+        if value and fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        '''covert byte to string'''
+        return self._redis.get(key).decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        '''convert the bytes to int'''
+        return self.get(key, int)
