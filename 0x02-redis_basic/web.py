acff08a47e5a2ae951df64a7a3c0d534c62e12b2
bion-slmn <bionsol25@gmail.com>
2024-01-24 22:21:28 +0000

    added if main

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,14 +9,14 @@ from typing import Callable
 def counter_fun(func: Callable) -> Callable:
     '''decorator to track how many times a url is called
     and cache but the cache expires in 10sec'''
-    redis_client = redis.Redis(decode_responses=True)
+    redis_client = redis.Redis()
     
     @wraps(func)
     def inner(url):
         '''innner function for wdecorator'''
         cached_html = redis_client.get(url)
         if cached_html:
-            return cached_html
+            return cached_html.decode("utf-8")
 
         key = 'count:' + url
         redis_client.incr(key)
@@ -32,3 +32,6 @@ def get_page(url: str) -> str:
     '''this function get a page and return the html of tha page'''
     html = requests.get(url).text
     return html
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
