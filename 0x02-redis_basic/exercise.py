ba1a6bd192dde482e2c02b2b210e6a2aae8264af
bion-slmn <bionsol25@gmail.com>
2024-01-24 13:30:48 +0000

    improved the doc string

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,7 +13,13 @@ class Cache():
         self._redis.flushdb()
 
     def store(self, data: Union[int, str, float, bytes]) -> str:
-        '''this method generates a random key and store it redis'''
+        '''this method generates a random key and store it redis
+        Args:
+            data: the data to be stored in redis
+
+        Return:
+            str: the generated random key
+        '''
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
