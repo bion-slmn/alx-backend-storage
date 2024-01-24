cfb0fc38e30cb7abe50a791eb9251b6b3c9af7c2
bion-slmn <bionsol25@gmail.com>
2024-01-24 22:04:55 +0000

    implementing a cache

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+''' this module implements function track how many times a url is called'''
+
+import requests
+import redis
+from typing import Callable
+
+
+def counter_fun(func: Callable) -> None:
+    '''decorator to track how many times a url is called
+    and cache but the cache expires in 10sec'''
+    redis_client = redis.Redis(decode_responses=True)
+
+    def inner(url):
+
+        cached_html = redis_client.get(url)
+        if cached_html:
+            return cached_html
+
+        key = 'count:' + url
+        redis_client.incr(key)
+
+        result = func(url)
+        redis_client.set(url, result, ex=10)
+
+        return result
+    return inner
+
+
+@counter_fun
+def get_page(url: str) -> str:
+    '''this function get a page and return the html of tha page'''
+    html = requests.get(url).text
+    return html
